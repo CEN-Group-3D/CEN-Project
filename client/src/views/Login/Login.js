4e245935101acdbd1ad4131c1ff786b1c6aea10e
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-10-30 13:13:31 -0400

    Creates Login view

---
+++ client/src/views/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className=' container col-xs-12 col-md-3'>Yo</div>
+    );
+}
+
+export default Login;
