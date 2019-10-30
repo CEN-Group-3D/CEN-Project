968f8da6e47c9039f142571f432c69534852c5ed
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-10-30 13:40:39 -0400

    create working model of login page

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -3,7 +3,18 @@ import './Login.css';
 
 function Login() {
     return (
-        <div className=' container col-xs-12 col-md-3'>Yo</div>
+        <div className='panel container col-xs-12 col-md-3'>
+            <h2>Welcome back</h2>
+            <form className="login-form">
+                <label for="username">Username:
+                    <input type="text" id="username" name="username"></input>
+                </label>
+                <label for="password">Password:
+                    <input type="password" id="password" name="password"></input>
+                </label>
+                <button className="btn btn-primary">Log in</button>
+            </form>
+        </div>
     );
 }
 
