4c45e89adbbade76f793a420c8585c9e833051d1
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-10-30 13:13:22 -0400

    Add bootstrap and default styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
+import Login from "./views/Login/Login"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
@@ -10,6 +13,7 @@ const App = () => {
     <div>
       <Header />
       <Switch>
+        <Route exact path="/login" component={Login} />
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
           <Redirect to="/Home" />
