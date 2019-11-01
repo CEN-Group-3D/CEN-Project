ad8493cdc88f1eba0b2f492107bfd635fcde2369
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-01 00:36:08 -0400

    Adds empty Welcome view

---
+++ client/src/views/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+    return (
+        <div className='panel container col-xs-12 col-md-3'>
+            
+        </div>
+    );
+}
+
+export default Welcome;
