87f8eba3901602d3470139f39494afd1d231daeb
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-01 00:48:39 -0400

    Add HTML skeleton

--- client/src/views/Welcome/Welcome.js
+++ client/src/views/Welcome/Welcome.js
@@ -4,7 +4,20 @@ import './Welcome.css';
 function Welcome() {
     return (
         <div className='panel container col-xs-12 col-md-3'>
-            
+            <h1>Welcome to EstatePlanR!</h1>
+            <form>
+                <div id="question-field">
+                    <label for="estate-plans">Do you have any estate plans?</label>
+                    <select name="estate-plans">
+                        <option value="none">No</option>
+                        <option value="poa">Power of Attorney</option>
+                        <option value="lwill">Living Will</option>
+                        <option value="will">Will</option>
+                        <option value="trust">Trust</option>
+                    </select>
+                </div>
+                <button className="btn btn-primary">Continue</button>
+            </form>
         </div>
     );
 }
