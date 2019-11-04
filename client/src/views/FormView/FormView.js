c5e2047b97b9d27d15527cc6b6f55265c8c02b11
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-04 18:27:20 -0500

    form view init

---
+++ client/src/views/FormView/FormView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FormView.css';
+
+function Home() {
+    return (
+        <div className="col-xs-12 col-md-6">
+            <form className="container">
+                <div className="panel col-xs-12">
+                    
+                </div> 
+            </form>
+        </div>
+    );
+}
+
+export default Home;
