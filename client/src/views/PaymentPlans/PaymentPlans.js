45f824c2f80ad89516b54650053b90f66630cd61
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-04 22:33:04 -0500

    converts all views into components

--- client/src/views/PaymentPlans/PaymentPlans.js
+++ client/src/views/PaymentPlans/PaymentPlans.js
@@ -2,52 +2,59 @@ import React from 'react';
 import Redirect from 'react';
 import './PaymentPlans.css';
 
-function PaymentPlans() {
-    return (
-        <div className="plans-container container col-xs-12 col-md-7">
-            <div className="row">
-                <div className='col-xs-12 col-md-4'>
-                    <div className="panel plan-container">
-                        <h3>Simple</h3>
-                        <span className="currency">$</span><span className="price">10</span><span className="period">/mo</span>
-                        <ul className="featuers-list">
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                        </ul>
-                        <button className="btn btn-outline-primary">Select this plan</button>
+class PaymentPlans extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="plans-container container col-xs-12 col-md-7">
+                <div className="row">
+                    <div className='col-xs-12 col-md-4'>
+                        <div className="panel plan-container">
+                            <h3>Simple</h3>
+                            <span className="currency">$</span><span className="price">10</span><span className="period">/mo</span>
+                            <ul className="featuers-list">
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                            </ul>
+                            <button className="btn btn-outline-primary">Select this plan</button>
+                        </div>
                     </div>
-                </div>
-                <div className='col-xs-12 col-md-4'>
-                    <div className="panel plan-container">
-                        <h3>Advanced</h3>
-                        <span className="currency">$</span><span className="price">20</span><span className="period">/mo</span>
-                        <ul className="featuers-list">
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                        </ul>
-                        <button className="btn btn-primary">Select this plan</button>
+                    <div className='col-xs-12 col-md-4'>
+                        <div className="panel plan-container">
+                            <h3>Advanced</h3>
+                            <span className="currency">$</span><span className="price">20</span><span className="period">/mo</span>
+                            <ul className="featuers-list">
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                            </ul>
+                            <button className="btn btn-primary">Select this plan</button>
+                        </div>
                     </div>
-                </div>
-                <div className='col-xs-12 col-md-4'>
-                    <div className="panel plan-container">
-                        <h3>Comprehensive</h3>
-                        <span className="currency">$</span><span className="price">30</span><span className="period">/mo</span>
-                        <ul className="featuers-list">
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                            <li>Short desc of feature</li>
-                        </ul>
-                        <button className="btn btn-primary">Select this plan</button>
+                    <div className='col-xs-12 col-md-4'>
+                        <div className="panel plan-container">
+                            <h3>Comprehensive</h3>
+                            <span className="currency">$</span><span className="price">30</span><span className="period">/mo</span>
+                            <ul className="featuers-list">
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                                <li>Short desc of feature</li>
+                            </ul>
+                            <button className="btn btn-primary">Select this plan</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default PaymentPlans;
