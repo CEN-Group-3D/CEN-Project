14db956f1a72b425e06e8feea71dcaf0a1013c5f
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-05 13:17:43 -0500

    UserDashboard init

---
+++ client/src/views/UserDashboardView/UserDashboardView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UserDashboardView.css';
+
+class UserDashboardView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        
+    }
+}
+export default Login;
