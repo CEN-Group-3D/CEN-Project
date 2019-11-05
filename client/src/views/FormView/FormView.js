ab2de871f863bd25c9e79381062b1dcbb27214a8
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-04 19:23:55 -0500

    Add first section to form

--- client/src/views/FormView/FormView.js
+++ client/src/views/FormView/FormView.js
@@ -5,8 +5,42 @@ function Home() {
     return (
         <div className="col-xs-12 col-md-6">
             <form className="container">
+                <h1>Personal and Family</h1>
                 <div className="panel col-xs-12">
-                    
+                    <div className="form-entry">
+                        <label for="fname">First Name</label>
+                        <input name="fname" type="text"></input>
+                    </div>
+                    <div className="form-entry">
+                        <label for="mname">Middle Initial</label>
+                        <input name="mname" type="text"></input>
+                    </div>
+                    <div className="form-entry">
+                        <label for="lname">Last Name</label>
+                        <input name="lname" type="text"></input>
+                    </div>
+                    <div className="form-entry">
+                        <label for="dob">First Name</label>
+                        <input name="dob" type="date"></input>
+                    </div>
+                    <div className="form-entry">
+                        <label for="gender">Gender</label>
+                        <select name="gender">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="transgender">Transgender</option>
+                            <option value="gender-neutral">Gender Neutral</option>
+                        </select>
+                    </div>
+                    <div className="form-entry">
+                        <label for="marital-status">Marital Status</label>
+                        <select name="marital-status">
+                            <option value="single">Single</option>
+                            <option value="married">Married</option>
+                            <option value="divorced">Divorced</option>
+                            <option value="widowed">Widowed</option>
+                        </select>
+                    </div>
                 </div> 
             </form>
         </div>
