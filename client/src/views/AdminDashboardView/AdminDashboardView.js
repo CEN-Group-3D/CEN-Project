c95b07ae67b08a840ccdb36da84ce5e11b2e8a4e
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-05 19:31:49 -0500

    tab switching now works

--- client/src/views/AdminDashboardView/AdminDashboardView.js
+++ client/src/views/AdminDashboardView/AdminDashboardView.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Tabs } from '../../components/Tabs/Tabs';
+import Tabs from '../../components/Tabs/Tabs';
 import './AdminDashboardView.css'
 
 class AdminDashboardView extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            selectedTab: 0,
+        };
+    }
+
+    tabComponents = [<p>hey</p>, <p>hello</p>]
+
+    displayNewTab = (index) => {
+        this.setState({
+            selectedTab: index,
+        })
     }
 
     render() {
         return (
             <div className="panel container">
-
+                <Tabs
+                    titles={['Users', 'Forms']}
+                    handleTabChangeCallback={this.displayNewTab}
+                />
+                { this.tabComponents[this.state.selectedTab] }
             </div>
         )
     }
