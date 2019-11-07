608439581157f0ceac40b4ee1ed8a958e98815aa
tama853 <16545536+tama853@users.noreply.github.com>
2019-11-07 18:15:19 -0500

    database access config.js

---
+++ server/config/config.example.js
@@ -0,0 +1,10 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  db: {
+    uri: // "add string for database access
+  }, 
+  port: 8080
+};
