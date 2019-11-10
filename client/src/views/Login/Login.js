a0cc21800dddaf5c95f9e63db56a5fadddcb8c8a
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-09 19:01:47 -0500

    handle post data programmatically

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -8,13 +8,27 @@ class Login extends React.Component {
         this.state = {};
     }
     
+    handleLogin = (evt) => {
+        evt.preventDefault();
+        let username = evt.target.username.value;
+        let password = evt.target.password.value;
+        
+        let loginData = new FormData();
+        loginData.append('username', username);
+        loginData.append('password', password);
+        fetch('/login', {
+            method: 'POST',
+            body: loginData,
+        })
+    }
+
     render() {
         return (
             <div className='login-panel panel container col-xs-12 col-md-3'>
                 <h2 id="login-header">Welcome back</h2>
-                <form className="login-form">
+                <form  method="POST" className="login-form">
                     <div className="login-field">
-                        <label for="username">Username</label>
+                        <label for="username">Email</label>
                         <input type="text" id="username" name="username"></input>
                     </div>
                     <div className="login-field">
