bacb34d8737f5a2e5abba027e259c4933aecf7cb
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-11 01:49:15 -0500

    moved form validation to onSubmit

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -10,26 +10,20 @@ class Register extends React.Component {
         };
     }
 
-    componentDidMount() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        let forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, (form) => {
-            form.addEventListener('submit', function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
+    checkInput = () => {
+
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        evt.target.classList.add('was-validated');
     }
 
     render() {
         return (
             <div className='panel container' id="login-panel">
                 <h2 id="login-header">Create an account</h2>
-                <form method="POST" className="login-form needs-validation" noValidate>
+                <form onSubmit={this.handleSubmit} method="POST" className="login-form needs-validation" noValidate>
                     {/* {
                         this.state.error ? 
                             <div className="alert alert-danger">The email and/or password is incorrect!</div> : 
@@ -47,11 +41,11 @@ class Register extends React.Component {
                     </div>
                     <div className="form-group login-field">
                         <label htmlFor="password">Password</label>
-                        <input required minLenght="6" className="form-control" type="password" id="password" name="password"></input>
+                        <input required minLength="6" className="form-control" type="password" id="password" name="password"></input>
+                        <div className="invalid-feedback">Please provide a valid password.</div>
                         <small className="form-text text-muted">
                             Your password must have at least 6 characters
                         </small>
-                        <div className="invalid-feedback">Please provide a password.</div>
                     </div>
                     <div className="form-group login-field">
                         <label htmlFor="confirm-password">Confirm password</label>
