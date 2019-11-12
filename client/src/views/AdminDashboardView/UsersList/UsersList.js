d78562d0ab0965a4f32b3e76be7f3022e3725fd0
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-11 23:56:25 -0500

    creates UsersList component

---
+++ client/src/views/AdminDashboardView/UsersList/UsersList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import './UsersList.css';
+
+class UsersList extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+UsersList.propTypes = {
+
+}
+export default UsersList
