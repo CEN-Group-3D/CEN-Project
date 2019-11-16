eca506914a30b2d3cc42ce972fcda64ebf2967e5
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-11-16 00:44:43 -0500

    removed redux persist implementation

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {isLoggedIn: false};
-
 const middleware = [thunk];
 
 const store = createStore(
