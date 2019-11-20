3da3042bd7488c571c0cdab0a847c4afe61b164f
David Mara <dmara34@gmail.com>
2019-11-20 08:46:39 -0500

    export pdf and POA.js

---
+++ client/src/views/UserDashboardView/FormTemplates/POA.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+export let POA = 
+	<div>
+		<h1>Power Of Attorney - Limited</h1>
+		<p>I, [Your Name And Address], the undersigned, hereby appoint and make [Agent's Name And Address] as my attorney-in-fact who shall have full power and authority to represent me and act on my behalf for ONLY the following matters:</p>
+	</div>
