da02ce4a17876f7796b1bcfd394acf1342523bf4
David Mara <dmara34@gmail.com>
2019-11-30 11:58:42 -0500

    embedded fields in POA

---
+++ client/src/views/UserDashboardView/UserProfile/PrivateProfile.js
@@ -0,0 +1,23 @@
+//PrivateProfile.js
+import React, { Component } from 'react';
+export let PrivateProfile = {
+	name : "Default_Name",
+	address : "Default_Address",
+	agent : "Agent_Name",
+	agent_Address : "Agent_Address",
+	POA_Matters : {
+		one: "1. Decide medical care of the principal.",
+		two: "2. Which doctors and care providers the principal uses.",
+		three: "3. File taxes on behalf of the principal.",
+		four: "4. Manage the principal’s property."
+	},
+	effective_Date : "1/1/2020",
+	effective_After : "will",
+	state : "Florida",
+	signed_Day: "First",
+	signed_Month: "January",
+	signed_Year: "2020",
+	signature: "Default SIG",
+	SSN: "-&&&&",
+	country: "USA"
+}
