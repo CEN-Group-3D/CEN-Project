613013a817a679a90e37b69d6be672409851dda1
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-12-03 03:38:03 -0500

    applied some formrows

--- client/src/views/FormView/FormData.js
+++ client/src/views/FormView/FormData.js
@@ -405,29 +405,46 @@ export let survivorAndBeneficiary = {
     title: "Survivor and Beneficiary",
     fields: [
         {
-            label: "First Name of Spouse",
-            dataTag: "fnameSp",
-            type: "text",
+            formRow: true,
+            fields: [
+                {
+                    label: "First Name of Spouse",
+                    dataTag: "fnameSp",
+                    type: "text",
+                    length: 4
+                },
+                {
+                    label: "M.I",
+                    dataTag: "mnameSp",
+                    type: "text",
+                    length: 1
+                },
+                {
+                    label: "Last Name",
+                    dataTag: "lnameSp",
+                    type: "text",
+                    length: 4
+                },
+
+            ]
         },
         {
-            label: "Middle Initial",
-            dataTag: "mnameSp",
-            type: "text",
-        },
-        {
-            label: "Last Name",
-            dataTag: "lnameSp",
-            type: "text",
-        },
-	    {
-            label: "Relationship to Spouse",
-            dataTag: "relatSp",
-            type: "text",
-        },
-        {
-            label: "Telephone",
-            dataTag: "teleSp",
-            type: "tel",
+            formRow: true,
+            fields: [
+                {
+                    label: "Relationship to Spouse",
+                    dataTag: "relatSp",
+                    type: "text",
+                    length: 6
+                },
+                {
+                    label: "Telephone",
+                    dataTag: "teleSp",
+                    type: "tel",
+                    length: 6
+                },
+
+            ]
         },
         {
             label: "Address Line 1",
@@ -440,257 +457,266 @@ export let survivorAndBeneficiary = {
             type: "text",
         },
         {
-            label: "City",
-            dataTag: "citySp",
-            type: "text",
-        },
-        {
-            label: "State",
-            dataTag: "stateSp",
-            type: "option",
-            options: [
+            formRow: true,
+            fields: [
                 {
-                    name: "Alabama",
-                    value: "AL"
+                    label: "City",
+                    dataTag: "citySp",
+                    type: "text",
+                    length: 6
                 },
                 {
-                    name: "Alaska",
-                    value: "AK"
+                    label: "State",
+                    dataTag: "stateSp",
+                    type: "option",
+                    options: [
+                        {
+                            name: "Alabama",
+                            value: "AL"
+                        },
+                        {
+                            name: "Alaska",
+                            value: "AK"
+                        },
+                        {
+                            name: "American Samoa",
+                            value: "AS"
+                        },
+                        {
+                            name: "Arizona",
+                            value: "AZ"
+                        },
+                        {
+                            name: "Arkansas",
+                            value: "AR"
+                        },
+                        {
+                            name: "California",
+                            value: "CA"
+                        },
+                        {
+                            name: "Colorado",
+                            value: "CO"
+                        },
+                        {
+                            name: "Connecticut",
+                            value: "CT"
+                        },
+                        {
+                            name: "Delaware",
+                            value: "DE"
+                        },
+                        {
+                            name: "District Of Columbia",
+                            value: "DC"
+                        },
+                        {
+                            name: "Federated States Of Micronesia",
+                            value: "FM"
+                        },
+                        {
+                            name: "Florida",
+                            value: "FL"
+                        },
+                        {
+                            name: "Georgia",
+                            value: "GA"
+                        },
+                        {
+                            name: "Guam",
+                            value: "GU"
+                        },
+                        {
+                            name: "Hawaii",
+                            value: "HI"
+                        },
+                        {
+                            name: "Idaho",
+                            value: "ID"
+                        },
+                        {
+                            name: "Illinois",
+                            value: "IL"
+                        },
+                        {
+                            name: "Indiana",
+                            value: "IN"
+                        },
+                        {
+                            name: "Iowa",
+                            value: "IA"
+                        },
+                        {
+                            name: "Kansas",
+                            value: "KS"
+                        },
+                        {
+                            name: "Kentucky",
+                            value: "KY"
+                        },
+                        {
+                            name: "Louisiana",
+                            value: "LA"
+                        },
+                        {
+                            name: "Maine",
+                            value: "ME"
+                        },
+                        {
+                            name: "Marshall Islands",
+                            value: "MH"
+                        },
+                        {
+                            name: "Maryland",
+                            value: "MD"
+                        },
+                        {
+                            name: "Massachusetts",
+                            value: "MA"
+                        },
+                        {
+                            name: "Michigan",
+                            value: "MI"
+                        },
+                        {
+                            name: "Minnesota",
+                            value: "MN"
+                        },
+                        {
+                            name: "Mississippi",
+                            value: "MS"
+                        },
+                        {
+                            name: "Missouri",
+                            value: "MO"
+                        },
+                        {
+                            name: "Montana",
+                            value: "MT"
+                        },
+                        {
+                            name: "Nebraska",
+                            value: "NE"
+                        },
+                        {
+                            name: "Nevada",
+                            value: "NV"
+                        },
+                        {
+                            name: "New Hampshire",
+                            value: "NH"
+                        },
+                        {
+                            name: "New Jersey",
+                            value: "NJ"
+                        },
+                        {
+                            name: "New Mexico",
+                            value: "NM"
+                        },
+                        {
+                            name: "New York",
+                            value: "NY"
+                        },
+                        {
+                            name: "North Carolina",
+                            value: "NC"
+                        },
+                        {
+                            name: "North Dakota",
+                            value: "ND"
+                        },
+                        {
+                            name: "Northern Mariana Islands",
+                            value: "MP"
+                        },
+                        {
+                            name: "Ohio",
+                            value: "OH"
+                        },
+                        {
+                            name: "Oklahoma",
+                            value: "OK"
+                        },
+                        {
+                            name: "Oregon",
+                            value: "OR"
+                        },
+                        {
+                            name: "Palau",
+                            value: "PW"
+                        },
+                        {
+                            name: "Pennsylvania",
+                            value: "PA"
+                        },
+                        {
+                            name: "Puerto Rico",
+                            value: "PR"
+                        },
+                        {
+                            name: "Rhode Island",
+                            value: "RI"
+                        },
+                        {
+                            name: "South Carolina",
+                            value: "SC"
+                        },
+                        {
+                            name: "South Dakota",
+                            value: "SD"
+                        },
+                        {
+                            name: "Tennessee",
+                            value: "TN"
+                        },
+                        {
+                            name: "Texas",
+                            value: "TX"
+                        },
+                        {
+                            name: "Utah",
+                            value: "UT"
+                        },
+                        {
+                            name: "Vermont",
+                            value: "VT"
+                        },
+                        {
+                            name: "Virgin Islands",
+                            value: "VI"
+                        },
+                        {
+                            name: "Virginia",
+                            value: "VA"
+                        },
+                        {
+                            name: "Washington",
+                            value: "WA"
+                        },
+                        {
+                            name: "West Virginia",
+                            value: "WV"
+                        },
+                        {
+                            name: "Wisconsin",
+                            value: "WI"
+                        },
+                        {
+                            name: "Wyoming",
+                            value: "WY"
+                        }
+                    ],
+                    length: 2
                 },
                 {
-                    name: "American Samoa",
-                    value: "AS"
+                    label: "Postal Code",
+                    dataTag: "zipSp",
+                    type: "number",
+                    length: 4,
                 },
-                {
-                    name: "Arizona",
-                    value: "AZ"
-                },
-                {
-                    name: "Arkansas",
-                    value: "AR"
-                },
-                {
-                    name: "California",
-                    value: "CA"
-                },
-                {
-                    name: "Colorado",
-                    value: "CO"
-                },
-                {
-                    name: "Connecticut",
-                    value: "CT"
-                },
-                {
-                    name: "Delaware",
-                    value: "DE"
-                },
-                {
-                    name: "District Of Columbia",
-                    value: "DC"
-                },
-                {
-                    name: "Federated States Of Micronesia",
-                    value: "FM"
-                },
-                {
-                    name: "Florida",
-                    value: "FL"
-                },
-                {
-                    name: "Georgia",
-                    value: "GA"
-                },
-                {
-                    name: "Guam",
-                    value: "GU"
-                },
-                {
-                    name: "Hawaii",
-                    value: "HI"
-                },
-                {
-                    name: "Idaho",
-                    value: "ID"
-                },
-                {
-                    name: "Illinois",
-                    value: "IL"
-                },
-                {
-                    name: "Indiana",
-                    value: "IN"
-                },
-                {
-                    name: "Iowa",
-                    value: "IA"
-                },
-                {
-                    name: "Kansas",
-                    value: "KS"
-                },
-                {
-                    name: "Kentucky",
-                    value: "KY"
-                },
-                {
-                    name: "Louisiana",
-                    value: "LA"
-                },
-                {
-                    name: "Maine",
-                    value: "ME"
-                },
-                {
-                    name: "Marshall Islands",
-                    value: "MH"
-                },
-                {
-                    name: "Maryland",
-                    value: "MD"
-                },
-                {
-                    name: "Massachusetts",
-                    value: "MA"
-                },
-                {
-                    name: "Michigan",
-                    value: "MI"
-                },
-                {
-                    name: "Minnesota",
-                    value: "MN"
-                },
-                {
-                    name: "Mississippi",
-                    value: "MS"
-                },
-                {
-                    name: "Missouri",
-                    value: "MO"
-                },
-                {
-                    name: "Montana",
-                    value: "MT"
-                },
-                {
-                    name: "Nebraska",
-                    value: "NE"
-                },
-                {
-                    name: "Nevada",
-                    value: "NV"
-                },
-                {
-                    name: "New Hampshire",
-                    value: "NH"
-                },
-                {
-                    name: "New Jersey",
-                    value: "NJ"
-                },
-                {
-                    name: "New Mexico",
-                    value: "NM"
-                },
-                {
-                    name: "New York",
-                    value: "NY"
-                },
-                {
-                    name: "North Carolina",
-                    value: "NC"
-                },
-                {
-                    name: "North Dakota",
-                    value: "ND"
-                },
-                {
-                    name: "Northern Mariana Islands",
-                    value: "MP"
-                },
-                {
-                    name: "Ohio",
-                    value: "OH"
-                },
-                {
-                    name: "Oklahoma",
-                    value: "OK"
-                },
-                {
-                    name: "Oregon",
-                    value: "OR"
-                },
-                {
-                    name: "Palau",
-                    value: "PW"
-                },
-                {
-                    name: "Pennsylvania",
-                    value: "PA"
-                },
-                {
-                    name: "Puerto Rico",
-                    value: "PR"
-                },
-                {
-                    name: "Rhode Island",
-                    value: "RI"
-                },
-                {
-                    name: "South Carolina",
-                    value: "SC"
-                },
-                {
-                    name: "South Dakota",
-                    value: "SD"
-                },
-                {
-                    name: "Tennessee",
-                    value: "TN"
-                },
-                {
-                    name: "Texas",
-                    value: "TX"
-                },
-                {
-                    name: "Utah",
-                    value: "UT"
-                },
-                {
-                    name: "Vermont",
-                    value: "VT"
-                },
-                {
-                    name: "Virgin Islands",
-                    value: "VI"
-                },
-                {
-                    name: "Virginia",
-                    value: "VA"
-                },
-                {
-                    name: "Washington",
-                    value: "WA"
-                },
-                {
-                    name: "West Virginia",
-                    value: "WV"
-                },
-                {
-                    name: "Wisconsin",
-                    value: "WI"
-                },
-                {
-                    name: "Wyoming",
-                    value: "WY"
-                }
+
             ]
-        },
-        {
-            label: "Postal Code",
-            dataTag: "zipSp",
-            type: "number"
         },
         {
             label: "First Name of Domestic Partner",
