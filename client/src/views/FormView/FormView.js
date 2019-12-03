a259d4451ce23a972b7e723b004a4b3e0f2ba3e7
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-12-03 02:45:55 -0500

    redirect from form to dashboard

--- client/src/views/FormView/FormView.js
+++ client/src/views/FormView/FormView.js
@@ -114,7 +114,9 @@ class FormView extends React.Component {
             credentials: 'include',
             
         }).then((response) => {
-            
+            if (response.ok) {
+                window.location = '/dashboard'
+            }
         })
     }
 
@@ -124,43 +126,6 @@ class FormView extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     {this.generateForm(personalAndFamily)}
                     {this.generateForm(survivorAndBeneficiary)}
-                    {/* <h1>Personal and Family</h1>
-                    <div className="panel col-xs-12">
-                        <div className="form-entry">
-                            <label for="fname">First Name</label>
-                            <input id="fname" type="text"></input>
-                        </div>
-                        <div className="form-entry">
-                            <label for="mname">Middle Initial</label>
-                            <input id="mname" type="text"></input>
-                        </div>
-                        <div className="form-entry">
-                            <label for="lname">Last Name</label>
-                            <input id="lname" type="text"></input>
-                        </div>
-                        <div className="form-entry">
-                            <label for="dob">First Name</label>
-                            <input id="dob" type="date"></input>
-                        </div>
-                        <div className="form-entry">
-                            <label for="gender">Gender</label>
-                            <select id="gender">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="transgender">Transgender</option>
-                                <option value="gender-neutral">Gender Neutral</option>
-                            </select>
-                        </div>
-                        <div className="form-entry">
-                            <label for="marital-status">Marital Status</label>
-                            <select i="marital-status">
-                                <option value="single">Single</option>
-                                <option value="married">Married</option>
-                                <option value="divorced">Divorced</option>
-                                <option value="widowed">Widowed</option>
-                            </select>
-                        </div>
-                    </div>  */}
                     <div id="form-submit-container" className="container panel col-12">
                         <button className="btn btn-primary btn-lg btn-block">Submit</button>
                     </div>
