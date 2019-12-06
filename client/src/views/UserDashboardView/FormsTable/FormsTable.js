cdee969dcadd52272bdcf6bfc594d82da9463d94
Lucas Grinspan <lucasgrinspan@gmail.com>
2019-12-05 22:18:40 -0500

    added no plan message

--- client/src/views/UserDashboardView/FormsTable/FormsTable.js
+++ client/src/views/UserDashboardView/FormsTable/FormsTable.js
@@ -15,25 +15,32 @@ class FormsTable extends React.Component {
     render() { 
         return (
             <React.Fragment>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Last Modified</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.formsData.map((form, index) => {
-                            return (
-                                <FormEntry 
-                                    key={index}
-                                    title={form.title}
-                                    formID={form.id}
-                                />
-                            )
-                        })}
-                    </tbody>
-                </table>
+                {
+                    this.state.userPlan === 0 ?
+                        <p>You don't seem to have a plan. Purchase one <a href="/payments">here</a> to continue</p>
+                    :
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Last Modified</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.formsData.map((form, index) => {
+                                return (
+                                    <FormEntry 
+                                        key={index}
+                                        title={form.title}
+                                        formID={form.id}
+                                        formPlan={form.plan}
+                                        userPlan={this.state.userPlan}
+                                    />
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                }
             </React.Fragment>
         )
     }
